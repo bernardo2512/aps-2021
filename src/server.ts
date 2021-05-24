e3caffe0a4f704b2922ba915970571fcff9e86a1
Vitor Costa <deividevitor565@gmail.com>
2021-05-23 22:07:10 -0300

    Ajustes cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import "./database";
 import { routes } from "./routes";
+
+const cors = require('cors');
 const express = require("express")
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 app.use(routes);
+
 app.listen(3000, () => {
     console.log("Server is running in port 3000 ...")
 })
